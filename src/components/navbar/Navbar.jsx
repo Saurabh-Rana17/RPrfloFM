4fe21fe668e100ccae3ef6040895536a4d38e9a2
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2024-01-21 22:04:59 +0530

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,57 @@
+import { motion } from "framer-motion";
 import "./navbar.scss";
 import React from "react";
+
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* siebar */}
       <div className="wrapper">
-        <span>Saurabh Rana</span>
-        <div className="social">
-          <a href="#">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+        >
+          Saurabh Rana
+        </motion.span>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, scale: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              scale: 1,
+              y: 0,
+              transition: { staggerChildren: 0.2 },
+            },
+          }}
+          initial="hidden"
+          animate="visible"
+          className="social"
+        >
+          <motion.a
+            variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+            href="#"
+          >
             <img src="/facebook.png" alt="" />
-          </a>
-          <a href="#">
+          </motion.a>
+          <motion.a
+            variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+            href="#"
+          >
             <img src="/instagram.png" alt="" />
-          </a>
-          <a href="#">
+          </motion.a>
+          <motion.a
+            variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+            href="#"
+          >
             <img src="/youtube.png" alt="" />
-          </a>
-          <a href="#">
+          </motion.a>
+          <motion.a
+            variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+            href="#"
+          >
             <img src="/dribbble.png" alt="" />
-          </a>
-        </div>
+          </motion.a>
+        </motion.div>
       </div>
     </div>
   );
