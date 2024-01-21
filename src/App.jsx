b73fea2e3f4369faf0f493d39790ecfb7514ea0c
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2024-01-21 18:25:34 +0530

    scroll snap completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss";
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <section>
+        <a href="">Hero</a>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>contact</section>
+    </>
+  );
 };
 
 export default App;
